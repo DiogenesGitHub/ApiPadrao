6dc41cbfd99c7cf94e2bbbb5b0e5f018f0712373
LUCAS DIOGENES RIBEIRO <111573@pmenos.com.br>
2023-11-24 14:29:44 -0300

    Criação do HostedService

---
+++ ApiRestFull/HostedService/ProcessHostedService.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiRestFull.HostedService
+{
+    public class ProcessHostedService
+    {
+        private readonly ILogger<ProcessHostedService> _logger;
+
+        public ProcessHostedService(ILogger<ProcessHostedService> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
